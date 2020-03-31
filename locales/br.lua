5a0042bad45df9734d4885cd349170d3d3e45860
renildomarcio <krsolucoesweb@gmail.com>
2020-03-30 21:36:07 -0300

    Create br.lua

---
+++ locales/br.lua
@@ -0,0 +1,23 @@
+Locales['br'] = {
+    ['gunrange'] = " Gunrange ",
+    ['actionMessage'] = "Pressione ~INPUT_PICKUP~ para abrir o menu",
+    ['difficulty'] = " Dificuldade ",
+    ['easy'] = " Fácil ",
+    ['normal'] = " Normal ",
+    ['hard'] = " Difícil ",
+    ['harder'] = " Mais difíceis ",
+    ['impossible'] = " Impossível ",
+    ['targets'] = " Targets ",
+    ['points'] = " Pontos ",
+    ['you_got'] = "Você tem ",
+    ['wait_for_turn'] = "~o~ Aguarde a sua vez!",
+    ['point'] = " Pontos",
+
+    -- Scores
+    ['show_board'] = "Pressione ~INPUT_PICKUP~",
+    ['last_10'] = "ÚLTIMOS 10",
+    ['name'] = "Nome: ",
+    ['difficulty_2'] = "Dificuldade: ",
+    ['targets_2'] = "Targets: ",
+    ["points_2"] = "Pontos: "
+}
